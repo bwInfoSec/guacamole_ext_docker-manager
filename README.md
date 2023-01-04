2cbd831f133c4a73a0f4a0d6dd6484852d9c250e
Maximilian Schöneberger <maximilian.schoeneberger@urz.uni-heidelberg.de>
2023-01-04 10:25:54 +0100

    testing

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # guacamole_ext_docker-manager
 A Guacamole Extention, which listens for authentications and provides docker containers on demand.
+
+## requirements
+
+- maven
---
+++ guacamole-docker-manager/src/main/java/bwinfosec/de/guacamole/DockerManagerListener.java
@@ -0,0 +1,139 @@
+package bwinfosec.de.guacamole;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+import org.apache.guacamole.GuacamoleException;
+import org.apache.guacamole.net.auth.AuthenticatedUser;
+import org.apache.guacamole.net.auth.UserContext;
+import org.apache.guacamole.net.event.AuthenticationSuccessEvent;
+import org.apache.guacamole.net.event.listener.Listener;
+import org.apache.guacamole.protocol.GuacamoleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.ExposedPort;
+import com.github.dockerjava.api.model.HostConfig;
+import com.github.dockerjava.api.model.Ports;
+import com.github.dockerjava.core.DefaultDockerClientConfig;
+import com.github.dockerjava.core.DockerClientConfig;
+import com.github.dockerjava.core.DockerClientImpl;
+import com.github.dockerjava.httpclient5.ApacheDockerHttpClient;
+import com.github.dockerjava.transport.DockerHttpClient;
+
+/**
+ * TODO: Listen for authentication
+ *
+ */
+public class DockerManagerListener implements Listener {
+
+    private static final Logger logger = LoggerFactory.getLogger(DockerManagerListener.class);
+
+    class createDockerContainerThreaded implements Runnable {
+
+        String hostname;
+        String port;
+        String identifier;
+        String username;
+        String password;
+        String hiscout_url;
+
+        createDockerContainerThreaded(final String hostname, final String port, final String identifier, final String username, final String password) {
+            this.hostname = "10.10.11.70:2376";
+            this.port = port;
+            this.identifier = identifier;
+            this.username = username;
+            this.password = password;
+            this.hiscout_url = "www.hiscout.url";
+        }
+
+        public void run() {
+
+            DockerClientConfig config = DefaultDockerClientConfig.createDefaultConfigBuilder()
+                    //.withDockerHost(String.format("tcp://%s", hostname))
+                    .withDockerHost("tcp://10.10.11.70:2376")
+                    .withDockerCertPath("/home/debian/.docker/certs") //https://stackoverflow.com/questions/40134311/how-to-connect-to-a-docker-daemon-listening-to-ssl-tls-connections-over-hostvmi
+                    .withDockerTlsVerify(true)
+                    .build();
+
+            DockerHttpClient httpClient = new ApacheDockerHttpClient.Builder()
+                    .dockerHost(config.getDockerHost())
+                    .sslConfig(config.getSSLConfig())
+                    .maxConnections(100)
+                    .connectionTimeout(Duration.ofSeconds(30))
+                    .responseTimeout(Duration.ofSeconds(45))
+                    .build();
+
+            DockerClient dockerClient = DockerClientImpl.getInstance(config, httpClient);
+
+            List<Container> containers = dockerClient.listContainersCmd().exec();
+
+            Boolean containerExists = false;
+
+            for (Container container : containers) {
+                List<String> names = Arrays.asList(container.getNames());
+                if (names.contains(String.format("/term_%s_%s", identifier, username))) { // WHY THE FUCK /
+                    containerExists = true;
+                }
+            }
+
+            if (!containerExists) {
+
+                ExposedPort userRDP = ExposedPort.tcp(3389);
+                Ports portBindings = new Ports();
+                portBindings.bind(userRDP, Ports.Binding.bindPort(Integer.parseInt(port)));
+
+                CreateContainerResponse container = dockerClient
+                        .createContainerCmd("registry-gitlab.urz.uni-heidelberg.de/it-sec/molecule-docker-images/testing/ubuntu:rdesktop-ubuntu-kde")
+                        .withHostConfig(new HostConfig()
+                                .withPortBindings(portBindings))
+                        .withEnv(String.format("GUACAMOLE_USERNAME=%s", username),
+                                String.format("GUACAMOLE_PASSWORD=%s", password),
+                                String.format("HISCOUT_URL=%s", hiscout_url))
+                        .withName(String.format("term_%s_%s", identifier, username))
+                        .exec();
+
+                dockerClient.startContainerCmd(container.getId()).exec();
+            }
+        };
+    }
+
+    @Override
+    public void handleEvent(Object event) throws GuacamoleException {
+        if (event instanceof AuthenticationSuccessEvent) {
+
+            AuthenticatedUser user = ((AuthenticationSuccessEvent) event).getAuthenticatedUser();
+
+            String username = user.getCredentials().getUsername();
+            String password = user.getCredentials().getPassword();
+
+            UserContext context = user.getAuthenticationProvider().getUserContext(user);
+            Set<String> identifiers = context.getConnectionDirectory().getIdentifiers();
+
+            for (String identifier : identifiers) {
+                GuacamoleConfiguration config = context.getConnectionDirectory().get(identifier).getConfiguration();
+                String port = config.getParameter("port");
+                String hostname = config.getParameter("hostname");
+
+                logger.info("STARTING THREAD WITH: {}",
+                    config.getParameters().toString());
+    
+                Thread dockerCreateThread = new Thread(new createDockerContainerThreaded(hostname, port, identifier, username, password));
+                dockerCreateThread.start();
+            }
+
+            logger.info("successful authentication for user {}",
+                    ((AuthenticationSuccessEvent) event)
+                            .getCredentials().getPassword());
+
+        }
+
+        logger.info("received Guacamole event notification");
+    }
+
+}
